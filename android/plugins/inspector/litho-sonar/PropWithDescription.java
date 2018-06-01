5ecf0216f6ecf782080c803f0e0115667dbe5688
Daniel Buchele <daniel@buechele.cc>
2018-06-01 14:07:57 +0100

    publish website

--- android/plugins/inspector/litho-sonar/PropWithDescription.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
-package com.facebook.litho.sonar;
-
-public interface PropWithDescription {
-
-  Object getSonarLayoutInspectorPropDescription();
-}
