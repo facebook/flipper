88ecb9b4944d1d31525161dbd0d6201a32baa184
Pritesh Nandgaonkar <prit91@fb.com>
2018-06-18 18:45:37 +0100

    app builds but not with sonarfb

--- android/sample/src/sonar/com/facebook/sonar/sample/MainActivity.java
+++ android/sample/src/sonar/com/facebook/sonar/sample/MainActivity.java
@@ -4,19 +4,34 @@ package com.facebook.sonar.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import com.facebook.litho.ComponentContext;
-import com.facebook.litho.LithoView;
+import java.util.List;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+// import com.facebook.litho.ComponentContext;
+// import com.facebook.litho.LithoView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d("ON CREATE SAMPLE APP", "I m in Oncreate of sample app");
+//    final ComponentContext c = new ComponentContext(this);
+    // setContentView(
+    //     LithoView.create(
+    //         c,
+    //         RootComponent.create(c).build()));
+    LinearLayout linearLayout = new LinearLayout(this);
+     TextView ProgrammaticallyTextView = new TextView(this);
+     ProgrammaticallyTextView.setText("Hello World");
+     ProgrammaticallyTextView.setTextSize(22);
+     ProgrammaticallyTextView.setPadding(20, 300, 20, 100);
 
-    final ComponentContext c = new ComponentContext(this);
-    setContentView(
-        LithoView.create(
-            c,
-            RootComponent.create(c).build()));
+     linearLayout.addView(ProgrammaticallyTextView);
+
+     setContentView(linearLayout, new LinearLayout.LayoutParams(
+     LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
   }
 }
