edfdd6495035b936cb20a9fe313dfefc971fac08
Pritesh Nandgaonkar <prit91@fb.com>
2018-06-21 16:26:39 +0100

    [WIP] gradle builds but app freezes, updated fbjni so file is the possible culprit

--- android/sample/src/sonar/com/facebook/sonar/sample/MainActivity.java
+++ android/sample/src/sonar/com/facebook/sonar/sample/MainActivity.java
@@ -8,8 +8,8 @@ import java.util.List;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-// import com.facebook.litho.ComponentContext;
-// import com.facebook.litho.LithoView;
+import com.facebook.litho.ComponentContext;
+import com.facebook.litho.LithoView;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,21 +17,10 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    Log.d("ON CREATE SAMPLE APP", "I m in Oncreate of sample app");
-//    final ComponentContext c = new ComponentContext(this);
-    // setContentView(
-    //     LithoView.create(
-    //         c,
-    //         RootComponent.create(c).build()));
-    LinearLayout linearLayout = new LinearLayout(this);
-     TextView ProgrammaticallyTextView = new TextView(this);
-     ProgrammaticallyTextView.setText("Hello World");
-     ProgrammaticallyTextView.setTextSize(22);
-     ProgrammaticallyTextView.setPadding(20, 300, 20, 100);
-
-     linearLayout.addView(ProgrammaticallyTextView);
-
-     setContentView(linearLayout, new LinearLayout.LayoutParams(
-     LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+   final ComponentContext c = new ComponentContext(this);
+    setContentView(
+        LithoView.create(
+            c,
+            RootComponent.create(c).build()));
   }
 }
--- android/sample/src/sonar/com/facebook/sonar/sample/SonarSampleApplication.java
+++ android/sample/src/sonar/com/facebook/sonar/sample/SonarSampleApplication.java
@@ -10,54 +10,46 @@ import java.util.List;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-// import com.facebook.litho.sonar.LithoSonarDescriptors;
+import com.facebook.litho.sonar.LithoSonarDescriptors;
 import com.facebook.soloader.SoLoader;
 import com.facebook.sonar.plugins.inspector.DescriptorMapping;
 import com.facebook.sonar.plugins.inspector.InspectorSonarPlugin;
-// import com.facebook.sonar.android.utils.SonarUtils;
+import com.facebook.sonar.android.utils.SonarUtils;
 import com.facebook.sonar.android.AndroidSonarClient;
 import com.facebook.sonar.core.SonarClient;
-// import com.facebook.sonar.plugins.inspector.DescriptorMapping;
-// import com.facebook.sonar.plugins.inspector.InspectorSonarPlugin;
-// import com.facebook.sonar.plugins.network.NetworkSonarPlugin;
-// import com.facebook.sonar.plugins.network.SonarOkhttpInterceptor;
-// import com.facebook.sonar.plugins.network.NetworkResponseFormatter;
-// import okhttp3.OkHttpClient;
+import com.facebook.sonar.plugins.inspector.DescriptorMapping;
+import com.facebook.sonar.plugins.inspector.InspectorSonarPlugin;
+import com.facebook.sonar.plugins.network.NetworkSonarPlugin;
+import com.facebook.sonar.plugins.network.SonarOkhttpInterceptor;
+import com.facebook.sonar.plugins.network.NetworkResponseFormatter;
+import okhttp3.OkHttpClient;
 import java.util.concurrent.TimeUnit;
 
 public class SonarSampleApplication extends Application {
 
-  //static public OkHttpClient okhttpClient;
+  static public OkHttpClient okhttpClient;
 
   @Override
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, false);
-    try {
-        //SoLoader.loadLibrary("sonarfb");
-        SoLoader.loadLibrary("sonarfb");
-    } catch (Exception e) {
-        throw new RuntimeException(e);
-    }
-    // final SonarClient client = AndroidSonarClient.getInstance(this);
-    // final DescriptorMapping descriptorMapping = DescriptorMapping.withDefaults();
-    // client.addPlugin(new InspectorSonarPlugin(this, descriptorMapping));
-    // client.start();
-    // final DescriptorMapping descriptorMapping = DescriptorMapping.withDefaults();
-    //
-    // NetworkSonarPlugin networkPlugin = new NetworkSonarPlugin();
-    // SonarOkhttpInterceptor interceptor = new SonarOkhttpInterceptor(networkPlugin);
-    //
-    // okhttpClient = new OkHttpClient.Builder()
-    // .addNetworkInterceptor(interceptor)
-    // .connectTimeout(60, TimeUnit.SECONDS)
-    // .readTimeout(60, TimeUnit.SECONDS)
-    // .writeTimeout(10, TimeUnit.MINUTES)
-    // .build();
-    //
-    // //LithoSonarDescriptors.add(descriptorMapping);
-    // //client.addPlugin(new InspectorSonarPlugin(this, descriptorMapping));
-    // client.addPlugin(networkPlugin);
-    // client.start();
+
+    final SonarClient client = AndroidSonarClient.getInstance(this);
+    final DescriptorMapping descriptorMapping = DescriptorMapping.withDefaults();
+
+    NetworkSonarPlugin networkPlugin = new NetworkSonarPlugin();
+    SonarOkhttpInterceptor interceptor = new SonarOkhttpInterceptor(networkPlugin);
+
+    okhttpClient = new OkHttpClient.Builder()
+    .addNetworkInterceptor(interceptor)
+    .connectTimeout(60, TimeUnit.SECONDS)
+    .readTimeout(60, TimeUnit.SECONDS)
+    .writeTimeout(10, TimeUnit.MINUTES)
+    .build();
+
+    LithoSonarDescriptors.add(descriptorMapping);
+    client.addPlugin(new InspectorSonarPlugin(this, descriptorMapping));
+    client.addPlugin(networkPlugin);
+    client.start();
     }
 }
