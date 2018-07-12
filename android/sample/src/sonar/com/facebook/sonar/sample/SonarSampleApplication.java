4e7a746dcbeb684b0e94fae8070877ff8786fbfe
Pascal Hartig <phartig@rdrei.net>
2018-07-12 17:10:10 +0100

    Split native steps off into separate build file

--- android/sample/src/sonar/com/facebook/sonar/sample/SonarSampleApplication.java
+++ android/sample/src/sonar/com/facebook/sonar/sample/SonarSampleApplication.java
@@ -4,32 +4,21 @@ package com.facebook.sonar.sample;
 
 import android.app.Application;
 import android.content.Context;
-import android.net.Network;
-import android.support.annotation.Nullable;
-import com.facebook.sonar.plugins.sharedpreferences.SharedPreferencesSonarPlugin;
-import java.util.ArrayList;
-import java.util.List;
-import android.widget.LinearLayout.LayoutParams;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import com.facebook.litho.sonar.LithoSonarDescriptors;
 import com.facebook.soloader.SoLoader;
-import com.facebook.sonar.plugins.inspector.DescriptorMapping;
-import com.facebook.sonar.plugins.inspector.InspectorSonarPlugin;
-import com.facebook.sonar.android.utils.SonarUtils;
 import com.facebook.sonar.android.AndroidSonarClient;
 import com.facebook.sonar.core.SonarClient;
 import com.facebook.sonar.plugins.inspector.DescriptorMapping;
 import com.facebook.sonar.plugins.inspector.InspectorSonarPlugin;
 import com.facebook.sonar.plugins.network.NetworkSonarPlugin;
 import com.facebook.sonar.plugins.network.SonarOkhttpInterceptor;
-import com.facebook.sonar.plugins.network.NetworkResponseFormatter;
-import okhttp3.OkHttpClient;
+import com.facebook.sonar.plugins.sharedpreferences.SharedPreferencesSonarPlugin;
 import java.util.concurrent.TimeUnit;
+import okhttp3.OkHttpClient;
 
 public class SonarSampleApplication extends Application {
 
-  static public OkHttpClient okhttpClient;
+  public static OkHttpClient okhttpClient;
 
   @Override
   public void onCreate() {
