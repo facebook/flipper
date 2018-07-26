6c78895b2a690fdca3a5ed9e1014e820290a511b
Pritesh Nandgaonkar <prit91@fb.com>
2018-07-26 05:12:06 -0700

    Move SampleSwift project to xplat
    Summary: Move SampleSwift project to xplat
    
    Reviewed By: passy
    
    Differential Revision: D8989867
    
    fbshipit-source-id: cfda775ec3919d5c689aaf6846a870a2653c47a6

---
+++ iOS/SampleSwift/SampleSwift/ViewController.swift
@@ -0,0 +1,18 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
