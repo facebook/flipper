db8fa7902c68c067c6aa13e7c1556b0c9cb13fdf
Pascal Hartig <realpassy@fb.com>
2018-08-29 10:08:39 -0700

    Fix publish-android-snapshot issues
    Summary:
    As expected, this didn't work on the first try. Issues I've fixed:
    
    - Actually switch back to snapshots.
    - Use more widely compatible bash location.
    - Don't exit (because of `set -e`) if `grep` returns with status 1.
    
    Reviewed By: jknoxville
    
    Differential Revision: D9556770
    
    fbshipit-source-id: 0900e6498e7fc8e16c941e77927005573ca310d5

--- scripts/publish-android-snapshot.sh
+++ scripts/publish-android-snapshot.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 set -e
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-IS_SNAPSHOT="$(grep 'VERSION_NAME=[0-9\.]\+-SNAPSHOT' "$BASEDIR/gradle.properties")"
+IS_SNAPSHOT="$(grep 'VERSION_NAME=[0-9\.]\+-SNAPSHOT' "$BASEDIR/gradle.properties" || echo "")"
 
 if [ "$ANDROID_PUBLISH_KEY" == "" ]; then
   echo "No encryption key. Skipping snapshot deployment."
