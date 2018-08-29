52fe553e21d7d13461a695d4912088b17e6d6061
Pritesh Nandgaonkar <prit91@fb.com>
2018-08-29 06:01:04 -0700

    Change iOS version update script
    Summary: This diff updates the iOS version update script to incorporate the change in folder from fbobjc to xplat.
    
    Reviewed By: danielbuechele
    
    Differential Revision: D9542137
    
    fbshipit-source-id: 4cb48d1b14d3c94d000490c7a21260766d493fc7

--- scripts/public-ios-podspec-update.sh
+++ scripts/public-ios-podspec-update.sh
@@ -3,15 +3,13 @@
 echo "✨ Updating podspecs with new release..."
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-FBSOURCE_DIR="$DIR/../../../../"
-XPLAT_SONAR_DIR="$FBSOURCE_DIR/xplat/sonar"
-XPLAT_SONAR_GETTING_STARTED_DOC="$XPLAT_SONAR_DIR/docs/getting-started.md"
-XPLAT_SONAR_CLIENT_SONAR_PODSPEC="$FBSOURCE_DIR/xplat/sonar-client/Sonar/Sonar.podspec"
-SPECS_DIR="$FBSOURCE_DIR/xplat/sonar/Specs"
-PODSPEC_PATH="$DIR/../OSSProject/SonarKit.podspec"
-PODFILE_PATH="$DIR/../Sample/Podfile"
+SONAR_DIR="$DIR/../"
+SONARKIT_PODSPEC_PATH="$SONAR_DIR/iOS/SonarKit.podspec"
+SONAR_PODSPEC_PATH="$SONAR_DIR/xplat/Sonar/Sonar.podspec"
+SONAR_GETTING_STARTED_DOC="$SONAR_DIR/docs/getting-started.md"
+SPECS_DIR="$SONAR_DIR/Specs/"
 SONARKIT_VERSION_TAG='sonarkit_version'
-OLD_VERSION_POD_ARG=$(< "$PODSPEC_PATH" grep "$SONARKIT_VERSION_TAG =" )
+OLD_VERSION_POD_ARG=$(< "$SONAR_PODSPEC_PATH" grep "$SONARKIT_VERSION_TAG =" )
 OLD_VERSION="${OLD_VERSION_POD_ARG##* }"
 
 echo "Currently released version is $OLD_VERSION, What should the version of the next release be?"
@@ -20,13 +18,12 @@ read -r VERSION
 echo "Updating version $VERSION in podspecs, podfiles and in getting started docs..."
 
 # Update Podspec files and podfiles with correct version
-sed -i "" "s/${SONARKIT_VERSION_TAG} = ${OLD_VERSION}/${SONARKIT_VERSION_TAG} = '${VERSION}'/g" "$PODSPEC_PATH"
-sed -i "" "s/${SONARKIT_VERSION_TAG} = ${OLD_VERSION}/${SONARKIT_VERSION_TAG} = '${VERSION}'/g" "$PODFILE_PATH"
-sed -i "" "s/${SONARKIT_VERSION_TAG} = ${OLD_VERSION}/${SONARKIT_VERSION_TAG} = '${VERSION}'/g" "$XPLAT_SONAR_CLIENT_SONAR_PODSPEC"
-sed -i "" "s/${SONARKIT_VERSION_TAG} = ${OLD_VERSION}/${SONARKIT_VERSION_TAG} = '${VERSION}'/g" "$XPLAT_SONAR_GETTING_STARTED_DOC"
+sed -i "" "s/${SONARKIT_VERSION_TAG} = ${OLD_VERSION}/${SONARKIT_VERSION_TAG} = '${VERSION}'/g" "$SONARKIT_PODSPEC_PATH"
+sed -i "" "s/${SONARKIT_VERSION_TAG} = ${OLD_VERSION}/${SONARKIT_VERSION_TAG} = '${VERSION}'/g" "$SONAR_PODSPEC_PATH"
+sed -i "" "s/${SONARKIT_VERSION_TAG} = ${OLD_VERSION}/${SONARKIT_VERSION_TAG} = '${VERSION}'/g" "$SONAR_GETTING_STARTED_DOC"
 
 # Copy Podfiles
 mkdir "$SPECS_DIR/SonarKit/$VERSION"  # New Specs dir for SonarKit podspec
 mkdir "$SPECS_DIR/Sonar/$VERSION"     # New Specs dir for Sonar podspec
-cp "$PODSPEC_PATH" "$SPECS_DIR/SonarKit/$VERSION" # Copied SonarKit podspec
-cp "$XPLAT_SONAR_CLIENT_SONAR_PODSPEC" "$SPECS_DIR/Sonar/$VERSION" # Copied Sonar podspec
+cp "$SONARKIT_PODSPEC_PATH" "$SPECS_DIR/SonarKit/$VERSION" # Copied SonarKit podspec
+cp "$SONAR_PODSPEC_PATH" "$SPECS_DIR/Sonar/$VERSION" # Copied Sonar podspec
