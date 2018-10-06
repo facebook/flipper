630982190b268076a8ba31fa58bcea8baf04bf2f
Alex Langenfeld <alangenfeld@fb.com>
2018-10-06 11:03:21 -0700

    remove OculusDevice
    Summary: Pretty sure this was meant to get removed before open sourcing, it isn't referenced anywhere.
    
    Reviewed By: danielbuechele
    
    Differential Revision: D10202081
    
    fbshipit-source-id: 23d213b3a041acad85eb723d32b65f7da276003d

--- src/devices/WindowsDevice.js
+++ src/devices/WindowsDevice.js
@@ -9,7 +9,6 @@ import BaseDevice from './BaseDevice.js';
 
 export default class WindowsDevice extends BaseDevice {
   supportedPlugins = [];
-  icon = 'icons/oculus.png';
   os = 'windows';
 
   constructor() {
