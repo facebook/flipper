00c51c034fdb1f7efbf776c891310b6a387a896b
Marc Terns <tenxurz@gmail.com>
2018-10-07 19:22:14 -0500

    Added test harness for user defaults

---
+++ iOS/Sample/UserDefaultsViewController.h
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsViewController.h
+//  Sample
+//
+//  Created by Marc Terns on 10/7/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserDefaultsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
