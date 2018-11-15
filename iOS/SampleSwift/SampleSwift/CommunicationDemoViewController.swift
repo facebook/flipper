dca02600cd809ff03df6de287746bd3ed52201e5
Pritesh Nandgaonkar <prit91@fb.com>
2018-11-15 23:08:13 +0530

    Example Flipper Plugin in swift sample app

---
+++ iOS/SampleSwift/SampleSwift/CommunicationDemoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CommunicationDemoViewController.swift
+//  SampleSwift
+//
+//  Created by Pritesh Nandgaonkar on 11/15/18.
+//  Copyright © 2018 Noah Gilmore. All rights reserved.
+//
+
+import Foundation
