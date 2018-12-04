d331ec2f101bd0d31a7b5c85939892bfebf4089c
Daniel Büchele <danielbuechele@fb.com>
2018-12-04 03:35:43 -0800

    adding Android ReactPlugin
    Summary:
    Adding a a react-devtools plugin for Android.
    This doesn't do much, but announces the plugin to be available.
    To make this work on remote device, the hostname and port sent to the device needs to be set in the dev tools. This is isn't done yet, so this will only work for simulators.
    
    Reviewed By: jknoxville
    
    Differential Revision: D12897151
    
    fbshipit-source-id: b874549925c395ff6356f6745b9706b182d544dd

---
+++ android/src/main/java/com/facebook/flipper/plugins/react/ReactFlipperPlugin.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (c) 2018-present, Facebook, Inc.
+ *
+ *  This source code is licensed under the MIT license found in the LICENSE
+ *  file in the root directory of this source tree.
+ *
+ */
+package com.facebook.flipper.plugins.react;
+
+import android.support.annotation.Nullable;
+import com.facebook.flipper.core.FlipperConnection;
+import com.facebook.flipper.core.FlipperObject;
+import com.facebook.flipper.core.FlipperPlugin;
+import com.facebook.flipper.core.FlipperReceiver;
+import com.facebook.flipper.core.FlipperResponder;
+
+public class ReactFlipperPlugin implements FlipperPlugin {
+
+  public static final String ID = "React";
+  @Nullable private FlipperConnection mConnection;
+
+  @Override
+  public String getId() {
+    return ID;
+  }
+
+  @Override
+  public void onConnect(FlipperConnection connection) {
+    mConnection = connection;
+    connection.receive(
+        "config",
+        new FlipperReceiver() {
+          @Override
+          public void onReceive(final FlipperObject params, FlipperResponder responder) {
+            // set received host and port in react-native
+          }
+        });
+  }
+
+  @Override
+  public void onDisconnect() {
+    mConnection = null;
+  }
+
+  @Override
+  public boolean runInBackground() {
+    return true;
+  }
+}
