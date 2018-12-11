c6e0517640204182eb6c882f9e974d4beab8aa42
George Zahariev <gkz@fb.com>
2018-12-11 13:46:01 -0800

    Enable Flow Strict Local for 13 files in xplat/sonar
    Summary:
    First add a Flow Strict config, then enable Flow Strict Local for 13 files in xplat/sonar.
    
    each change can land seperately:
    drop-conflicts
    
    Reviewed By: passy
    
    Differential Revision: D13368791
    
    fbshipit-source-id: 5bc1f9d3a179f4b9a554cc8cc629db0fda468a36

--- static/compilePlugins.js
+++ static/compilePlugins.js
@@ -3,7 +3,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  * @format
- * @flow
+ * @flow strict-local
  */
 
 const path = require('path');
