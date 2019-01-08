97752a7b42741c62151f9c497d6c766b4b268cbe
Michael Lee <mzlee@fb.com>
2019-01-08 12:05:31 -0800

    Fix missing header include
    Summary: This test uses `folly::SingletonVault` implicitly included. Make it explicit
    
    Differential Revision: D13597582
    
    fbshipit-source-id: 511c2cdb097f80bb32d6738fd7f6f14fa55b6801

--- xplat/FlipperTests/FlipperConnectionManagerImplTerminationTests.cpp
+++ xplat/FlipperTests/FlipperConnectionManagerImplTerminationTests.cpp
@@ -1,13 +1,12 @@
-/*
- *  Copyright (c) 2018-present, Facebook, Inc.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
  */
-
 #include <Flipper/FlipperConnectionManagerImpl.h>
 #include <FlipperTestLib/ConnectionContextStoreMock.h>
+#include <folly/Singleton.h>
 
 #include <gtest/gtest.h>
 
