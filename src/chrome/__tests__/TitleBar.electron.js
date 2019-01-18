51ab27456a035a7d03d7150b30def7d7653cb211
Pascal Hartig <realpassy@fb.com>
2019-01-18 08:54:14 -0800

    Add basic TitleBar test
    Summary: Manually checked that the snapshot doesn't contain the auto updater.
    
    Reviewed By: jknoxville
    
    Differential Revision: D13715520
    
    fbshipit-source-id: 720fed1e9952b788bce3311ecb508cfcfdc1758a

---
+++ src/chrome/__tests__/TitleBar.electron.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2018-present Facebook.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * @format
+ */
+
+import TitleBar from '../TitleBar.js';
+import renderer from 'react-test-renderer';
+import reducers from '../../reducers/index.js';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+
+const mockStore = configureStore([])(reducers(undefined, {type: 'INIT'}));
+
+test('TitleBar is rendered', () => {
+  const component = renderer.create(
+    <Provider store={mockStore}>
+      <TitleBar
+        windowIsFocused={true}
+        leftSidebarVisible={false}
+        rightSidebarVisible={false}
+        rightSidebarAvailable={false}
+        toggleLeftSidebarVisible={() => {}}
+        toggleRightSidebarVisible={() => {}}
+        setActiveSheet={_sheet => {}}
+      />
+    </Provider>,
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
