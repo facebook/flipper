0daa5de428eedd79dd23c695c861d5ae77fda011
Pascal Hartig <realpassy@fb.com>
2019-01-21 06:03:44 -0800

    Set parameters to non-null by default
    Summary:
    This helps Kotlin nullity inference and *should* already be enforced
    by Infer/Eradicate. More details here: https://medium.com/square-corner-blog/non-null-is-the-default-58ffc0bb9111
    
    Reviewed By: jknoxville
    
    Differential Revision: D13732788
    
    fbshipit-source-id: c0c60f0f8e9dc8bffe3780c7a1a700d682a6d2b8

---
+++ android/src/main/java/com/facebook/flipper/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+@javax.annotation.ParametersAreNonnullByDefault
+package com.facebook.flipper;
