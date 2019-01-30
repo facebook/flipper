f12226ac005cf5b5f2f29b63b14a9b6d3057c843
Pritesh Nandgaonkar <prit91@fb.com>
2019-01-30 06:45:55 -0800

    Just trigger for fatal logs and error logs with tag AndroidRuntime
    Summary: This diff updates the check which triggers crash notifications. We got a user feedback that crash reporter used to report non fatal crash too and it used to annoy users with bombardment of crash notifications.
    
    Reviewed By: passy
    
    Differential Revision: D13878272
    
    fbshipit-source-id: 75446f08f806e8f28a6f68953c0a001fd18a7dc0

--- src/utils/crashReporterUtility.js
+++ src/utils/crashReporterUtility.js
@@ -11,17 +11,9 @@ export function shouldParseAndroidLog(
   entry: DeviceLogEntry,
   date: Date,
 ): boolean {
-  const tagsCheck =
-    entry.tag === 'art' || // This tag is for jni errors from sample app
-    entry.tag === 'AndroidRuntime' || // This tag is for runtime java errors
-    entry.tag === 'libfbjni' || // Related to fbjni errors from flipper
-    entry.tag.includes('flipper');
-
-  const messagesCheck = entry.message.includes('flipper');
-
   return (
     entry.date.getTime() - date.getTime() > 0 && // The log should have arrived after the device has been registered
-    entry.type === 'error' &&
-    (tagsCheck || messagesCheck)
+    ((entry.type === 'error' && entry.tag === 'AndroidRuntime') ||
+      entry.type === 'fatal')
   );
 }
