861c5077f19e41153b6a35d799a9592790565ebb
Arthur Kushka <arhelmus@fb.com>
2019-04-05 05:37:36 -0700

    Extended ConnectionMock functionality
    Summary: Its helpful to store all history of sent messages for testing purposes.
    
    Reviewed By: jknoxville
    
    Differential Revision: D14797191
    
    fbshipit-source-id: 0e58166cb4ab79852165f2107b2f6bcd259f1096

--- xplat/FlipperTestLib/FlipperConnectionMock.h
+++ xplat/FlipperTestLib/FlipperConnectionMock.h
@@ -1,15 +1,14 @@
-/*
- *  Copyright (c) 2018-present, Facebook, Inc.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
  */
-
 #pragma once
 
 #include <Flipper/FlipperConnection.h>
 #include <map>
+#include <queue>
 #include <string>
 
 namespace facebook {
@@ -19,6 +18,7 @@ class FlipperConnectionMock : public FlipperConnection {
  public:
   void send(const std::string& method, const folly::dynamic& params) override {
     sent_[method] = params;
+    sent_message_history[method].push(params);
   }
 
   void receive(const std::string& method, const FlipperReceiver& receiver)
@@ -31,6 +31,8 @@ class FlipperConnectionMock : public FlipperConnection {
 
   std::map<std::string, folly::dynamic> sent_;
   std::map<std::string, FlipperReceiver> receivers_;
+
+  std::map<std::string, std::queue<folly::dynamic>> sent_message_history;
 };
 
 } // namespace flipper
