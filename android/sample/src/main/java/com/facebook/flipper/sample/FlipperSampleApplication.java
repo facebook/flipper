9a6ad643b3f681725d386ea226d2adaebdca43f8
Arnaud Frugier <arnaudf9@fb.com>
2019-04-10 14:59:37 -0700

    Separate both databases and add more sample data

--- android/sample/src/main/java/com/facebook/flipper/sample/FlipperSampleApplication.java
+++ android/sample/src/main/java/com/facebook/flipper/sample/FlipperSampleApplication.java
@@ -38,53 +38,125 @@ public class FlipperSampleApplication extends Application {
         .putInt("SomeKey", 1337)
         .apply();
 
-    DatabaseHelper db1Helper = new DatabaseHelper(this, "database1.db");
-    DatabaseHelper db2Helper = new DatabaseHelper(this, "database2.db");
+    Database1Helper db1Helper = new Database1Helper(this);
+    Database2Helper db2Helper = new Database2Helper(this);
 
-    long numEntries = DatabaseUtils.queryNumEntries(db1Helper.getReadableDatabase(), "first_table", null, null);
-    if(numEntries == 0) {
-      for(int i = 0 ; i < 10 ; i++) {
-        ContentValues values = new ContentValues();
-        values.put("column1", "value_" + i);
-        values.put("column2", "value_2_" + i);
-        db1Helper.getWritableDatabase().insert("first_table", null, values);
-        db1Helper.getWritableDatabase().insert("second_table", null, values);
-        db2Helper.getWritableDatabase().insert("first_table", null, values);
-        db2Helper.getWritableDatabase().insert("second_table", null, values);
-      }
-    }
+    DatabaseUtils.queryNumEntries(db1Helper.getReadableDatabase(), "db1_first_table", null, null);
+    DatabaseUtils.queryNumEntries(db2Helper.getReadableDatabase(), "db2_first_table", null, null);
   }
 
-  public static class DatabaseHelper extends SQLiteOpenHelper {
-    // If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 1;
-      private static final String SQL_CREATE_FIRST_TABLE =
-          "CREATE TABLE " + "first_table" + " (" +
-              "_id INTEGER PRIMARY KEY," +
-              "column1 TEXT," +
-              "column2 TEXT)";
-      private static final String SQL_CREATE_SECOND_TABLE =
-          "CREATE TABLE " + "second_table" + " (" +
-              "_id INTEGER PRIMARY KEY," +
-              "column1 TEXT," +
-              "column2 TEXT)";
+  public static class Database1Helper extends SQLiteOpenHelper {
 
-    public DatabaseHelper(Context context, String databaseName) {
-      super(context, databaseName, null, DATABASE_VERSION);
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 4;
+    private static final String SQL_CREATE_FIRST_TABLE =
+        "CREATE TABLE " + "db1_first_table" + " (" +
+            "_id INTEGER PRIMARY KEY," +
+            "db1_col0_text TEXT," +
+            "db1_col1_integer INTEGER," +
+            "db1_col2_float FLOAT," +
+            "db1_col3_blob TEXT," +
+            "db1_col4_null TEXT DEFAULT NULL," +
+            "db1_col5 TEXT," +
+            "db1_col6 TEXT," +
+            "db1_col7 TEXT," +
+            "db1_col8 TEXT," +
+            "db1_col9 TEXT" +
+        ")";
+    private static final String SQL_CREATE_SECOND_TABLE =
+        "CREATE TABLE " + "db1_empty_table" + " (" +
+            "_id INTEGER PRIMARY KEY," +
+            "column1 TEXT," +
+            "column2 TEXT)";
+
+    public Database1Helper(Context context) {
+      super(context, "database1.db", null, DATABASE_VERSION);
     }
+
     public void onCreate(SQLiteDatabase db) {
       db.execSQL(SQL_CREATE_FIRST_TABLE);
       db.execSQL(SQL_CREATE_SECOND_TABLE);
+      insertSampleData(db);
     }
+
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
       // This database is only a cache for online data, so its upgrade policy is
       // to simply to discard the data and start over
       db.execSQL("DROP TABLE IF EXISTS first_table");
       db.execSQL("DROP TABLE IF EXISTS second_table");
+      db.execSQL("DROP TABLE IF EXISTS db1_first_table");
+      db.execSQL("DROP TABLE IF EXISTS db1_empty_table");
       onCreate(db);
     }
+
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
       onUpgrade(db, oldVersion, newVersion);
+    }
+
+    public void insertSampleData(SQLiteDatabase db) {
+        for(int i = 0 ; i < 100 ;i++) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("db1_col0_text", "Long text data for testing resizing");
+            contentValues.put("db1_col1_integer", 1000 + i);
+            contentValues.put("db1_col2_float", 1000.465f + i);
+            contentValues.put("db1_col3_blob", new byte[]{0, 0, 0, 1, 1, 0, 1, 1});
+            contentValues.put("db1_col5", "db_1_column5_value");
+            contentValues.put("db1_col6", "db_1_column6_value");
+            contentValues.put("db1_col7", "db_1_column7_value");
+            contentValues.put("db1_col8", "db_1_column8_value");
+            contentValues.put("db1_col9", "db_1_column9_value");
+            db.insert("db1_first_table", null, contentValues);
+        }
+    }
+  }
+
+  public static class Database2Helper extends SQLiteOpenHelper {
+
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 4;
+    private static final String SQL_CREATE_FIRST_TABLE =
+        "CREATE TABLE " + "db2_first_table" + " (" +
+            "_id INTEGER PRIMARY KEY," +
+            "column1 TEXT," +
+            "column2 TEXT)";
+    private static final String SQL_CREATE_SECOND_TABLE =
+        "CREATE TABLE " + "db2_second_table" + " (" +
+            "_id INTEGER PRIMARY KEY," +
+            "column1 TEXT," +
+            "column2 TEXT)";
+
+    public Database2Helper(Context context) {
+      super(context, "database2.db", null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+      db.execSQL(SQL_CREATE_FIRST_TABLE);
+      db.execSQL(SQL_CREATE_SECOND_TABLE);
+      insertSampleData(db);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+      // This database is only a cache for online data, so its upgrade policy is
+      // to simply to discard the data and start over
+      db.execSQL("DROP TABLE IF EXISTS first_table");
+      db.execSQL("DROP TABLE IF EXISTS second_table");
+      db.execSQL("DROP TABLE IF EXISTS db2_first_table");
+      db.execSQL("DROP TABLE IF EXISTS db2_second_table");
+      onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+      onUpgrade(db, oldVersion, newVersion);
+    }
+
+    public void insertSampleData(SQLiteDatabase db) {
+        for(int i = 0 ; i < 10 ;i++) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("column1", "Long text data for testing resizing");
+            contentValues.put("column2", "extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra extra Long text data for testing resizing");
+            db.insert("db2_first_table", null, contentValues);
+            db.insert("db2_second_table", null, contentValues);
+        }
     }
   }
 }
