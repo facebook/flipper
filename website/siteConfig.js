eca569db421c191b246b0a3b4c8bd3648e5a0c1a
Pascal Hartig <realpassy@fb.com>
2019-04-30 03:38:18 -0700

    Set up Algolia search
    Summary: Registered the site and it got indexed over night. Yay!
    
    Reviewed By: jknoxville
    
    Differential Revision: D15146529
    
    fbshipit-source-id: 98a9ca68281d6b79f99a1da5d6f681340cb20ea1

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -33,6 +33,13 @@ const siteConfig = {
   highlight: {
     theme: 'default',
   },
+  algolia: {
+    apiKey: '2df980e7ffc95c19552790f7cad32666',
+    indexName: 'fbflipper',
+    algoliaOptions: {
+      hitsPerPage: 5,
+    },
+  },
   scripts: [
     'https://buttons.github.io/buttons.js',
     'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
