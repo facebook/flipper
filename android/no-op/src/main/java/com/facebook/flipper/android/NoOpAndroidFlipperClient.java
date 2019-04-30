db5d486c6f936dfc1425ae8d495ee4ae9608e16b
Pascal Hartig <realpassy@fb.com>
2019-04-30 13:08:52 -0700

    Set up no-op package (#425)
    Summary:
    Pull Request resolved: https://github.com/facebook/flipper/pull/425
    
    Incomplete implementation that should allow for most uses
    as a release-only package.
    
    Next steps:
    - Integrate sample app with this.
    - Set up CI to build sample app in release mode with this.
    - Register with JCenter.
    - Automatically publish to JCenter as part of our release step.
    
    Reviewed By: jknoxville
    
    Differential Revision: D15146823
    
    fbshipit-source-id: 3ad058dce7b0395721c6e6715d44d4d51b1834da

---
+++ android/no-op/src/main/java/com/facebook/flipper/android/NoOpAndroidFlipperClient.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) Facebook, Inc.
+ *
+ *  This source code is licensed under the MIT license found in the LICENSE
+ *  file in the root directory of this source tree.
+ *
+ */
+package com.facebook.flipper.android;
+
+import com.facebook.flipper.core.FlipperClient;
+import com.facebook.flipper.core.FlipperPlugin;
+import javax.annotation.Nullable;
+
+public class NoOpAndroidFlipperClient implements FlipperClient {
+
+  @Override
+  public void addPlugin(FlipperPlugin plugin) {
+    // no-op
+  }
+
+  @Nullable
+  @Override
+  public <T extends FlipperPlugin> T getPlugin(String id) {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public <T extends FlipperPlugin> T getPluginByClass(Class<T> cls) {
+    return null;
+  }
+
+  @Override
+  public void removePlugin(FlipperPlugin plugin) {
+    // no-op
+  }
+
+  @Override
+  public void start() {
+    // no-op
+  }
+
+  @Override
+  public void stop() {
+    // no-op
+  }
+
+  @Override
+  public void unsubscribe() {
+    // no-op
+  }
+}
