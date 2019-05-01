3339944ff4b962759722f0291d6caf2dd8ecc330
Pascal Hartig <realpassy@fb.com>
2019-05-01 09:35:45 -0700

    Tutorial app
    Summary:
    A very basic app with a scrolling list backed by a static data source. It also covers two bases that we hadn't before: Sections and Kotlin; the latter being the better choice for a modern Android tutorial anyway.
    
    Missing right now: BUCK support (likely not going to come anytime soon due to the kapt limitations), the actual Flipper plugin integration (that's up next).
    
    Reviewed By: jknoxville
    
    Differential Revision: D15166195
    
    fbshipit-source-id: 3cfaa1d243548279cabc4f244c13363f1bcaa36c

---
+++ android/tutorial/src/main/java/com/facebook/flipper/sample/tutorial/TutorialApplication.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the LICENSE file
+ * in the root directory of this source tree.
+ */
+
+package com.facebook.flipper.sample.tutorial
+
+import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.core.FlipperClient
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.litho.LithoFlipperDescriptors
+import com.facebook.soloader.SoLoader
+
+class TutorialApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        SoLoader.init(this, false)
+        Fresco.initialize(this)
+        val flipperClient = AndroidFlipperClient.getInstance(this)
+        val descriptorMapping = DescriptorMapping.withDefaults()
+        LithoFlipperDescriptors.addWithSections(descriptorMapping)
+
+        flipperClient.addPlugin(InspectorFlipperPlugin(this, descriptorMapping))
+        flipperClient.start()
+    }
+}
