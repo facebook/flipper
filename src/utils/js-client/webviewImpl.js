46e0abecdf4a5dc188619420ecd3938abc909f2a
Timur Valiev <timurvaliev@fb.com>
2019-08-14 03:26:04 -0700

    api + plugins examples
    Summary:
    Initial commit for Flipper API for JS apps:
    1) Plugin, Connection, Client
    2) a bridge to work with WebView proxy app
    3) examples of ported plugins: AnalyticsLogging and Fury
    
    Reviewed By: danielbuechele
    
    Differential Revision: D16753405
    
    fbshipit-source-id: cdd4b863db236b2043f09833aed130035f6738de

---
+++ src/utils/js-client/webviewImpl.js
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2018-present Facebook.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * @format
+ */
+
+import {FlipperClient, FlipperBridge} from './api';
+
+import type {FlipperPluginID, FlipperMethodID} from './api';
+
+class FlipperWebviewBridgeImpl extends FlipperBridge {
+  _subscriptions: Map<string, (any) => void> = new Map();
+
+  registerPlugins = (plugins: Array<FlipperPluginID>) => {
+    window.FlipperWebviewBridge &&
+      window.FlipperWebviewBridge.registerPlugins(plugins);
+  };
+
+  start = () => {
+    window.FlipperWebviewBridge && window.FlipperWebviewBridge.start();
+  };
+
+  stop = () => {
+    window.FlipperWebviewBridge && window.FlipperWebviewBridge.stop();
+  };
+
+  sendData = (plugin: FlipperPluginID, method: FlipperMethodID, data: any) => {
+    window.FlipperWebviewBridge &&
+      window.FlipperWebviewBridge.sendFlipperObject(
+        plugin,
+        method,
+        JSON.stringify(data),
+      );
+  };
+
+  subscribe = (
+    plugin: FlipperPluginID,
+    method: FlipperMethodID,
+    handler: any => void,
+  ) => {
+    this._subscriptions.set(plugin + method, handler);
+    window.FlipperWebviewBridge &&
+      window.FlipperWebviewBridge.subscribe(plugin, method);
+  };
+
+  isAvailable = () => {
+    return window.FlipperWebviewBridge != null;
+  };
+
+  handleMessage(plugin: FlipperPluginID, method: FlipperMethodID, data: any) {
+    const handler: ?(any) => void = this._subscriptions.get(plugin + method);
+    handler && handler(data);
+  }
+}
+
+export function newWebviewClient(): FlipperClient {
+  const bridge = new FlipperWebviewBridgeImpl();
+  window.FlipperBridgeClientSide = bridge;
+  return new FlipperClient(bridge);
+}
