367141ecad025c1b0d416ad95791849e5e4fc225
Pritesh Nandgaonkar <prit91@fb.com>
2019-08-16 19:28:35 +0100

    Sync FBCxxFolly

--- iOS/FlipperKit/FBCxxFollyDynamicConvert/FBCxxFollyDynamicConvert.h
+++ iOS/FlipperKit/FBCxxFollyDynamicConvert/FBCxxFollyDynamicConvert.h
@@ -1,1 +1,14 @@
-../../../../../fbobjc/Libraries/FBCxxUtils/FBCxxFollyDynamicConvert/FBCxxFollyDynamicConvert.h
+// Copyright 2004-present Facebook. All Rights Reserved.
+#pragma once
+
+#import <Foundation/Foundation.h>
+
+#include <folly/dynamic.h>
+
+namespace facebook {
+namespace cxxutils {
+
+folly::dynamic convertIdToFollyDynamic(id json, bool nullifyNanAndInf = false);
+id convertFollyDynamicToId(const folly::dynamic &dyn);
+
+} }
