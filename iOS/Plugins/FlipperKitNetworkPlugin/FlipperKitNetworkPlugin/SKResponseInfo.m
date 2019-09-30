c4c5d453c2560abe3a2bc5ef1e5d4b3283174e65
Andres Suarez <asuarez@fb.com>
2019-09-30 15:08:40 -0700

    Fix license headers
    Reviewed By: scottrice
    
    Differential Revision: D17673481
    
    fbshipit-source-id: 1794e0a6e92e6894b5634b24c6e7910847616261

--- iOS/Plugins/FlipperKitNetworkPlugin/FlipperKitNetworkPlugin/SKResponseInfo.m
+++ iOS/Plugins/FlipperKitNetworkPlugin/FlipperKitNetworkPlugin/SKResponseInfo.m
@@ -1,11 +1,9 @@
 /*
- *  Copyright (c) 2018-present, Facebook, Inc. and its affiliates.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- *  This source code is licensed under the MIT license found in the LICENSE
- *  file in the root directory of this source tree.
- *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
  */
-
 #import "SKResponseInfo.h"
 
 @implementation SKResponseInfo
