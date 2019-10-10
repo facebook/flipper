6909a4a3158fa2136d09adc2e40bd0c4e1ddf838
John Knox <jknox@fb.com>
2019-10-10 09:21:35 -0700

    Add snapshot tests for settings
    Summary: Take a snapshot with each field adjusted to prevent accidental changes.
    
    Reviewed By: priteshrnandgaonkar
    
    Differential Revision: D17832181
    
    fbshipit-source-id: 016f99ec032036993a40fea310c6fd9887da6c38

---
+++ src/chrome/__tests__/SettingsSheet.node.tsx
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2018-present Facebook.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * @format
+ */
+
+import SettingsSheet from '../SettingsSheet';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+
+function getStore({
+  iosEnabled,
+  androidEnabled,
+  androidHome,
+}: {
+  iosEnabled?: boolean;
+  androidEnabled?: boolean;
+  androidHome?: string;
+}) {
+  return configureStore([])({
+    application: {
+      sessionId: 'mysession',
+      xcodeCommandLineToolsDetected: iosEnabled,
+    },
+    settingsState: {
+      enableAndroid: androidEnabled,
+      androidHome: androidHome,
+    },
+  });
+}
+
+test('SettingsSheet snapshot with nothing enabled', () => {
+  const component = (
+    <Provider store={getStore({})}>
+      <SettingsSheet onHide={() => {}} />
+    </Provider>
+  );
+
+  expect(renderer.create(component).toJSON()).toMatchSnapshot();
+});
+
+test('SettingsSheet snapshot with iOS enabled', () => {
+  const component = (
+    <Provider store={getStore({iosEnabled: true})}>
+      <SettingsSheet onHide={() => {}} />
+    </Provider>
+  );
+
+  expect(renderer.create(component).toJSON()).toMatchSnapshot();
+});
+
+test('SettingsSheet snapshot with android enabled', () => {
+  const component = (
+    <Provider store={getStore({androidEnabled: true})}>
+      <SettingsSheet onHide={() => {}} />
+    </Provider>
+  );
+
+  expect(renderer.create(component).toJSON()).toMatchSnapshot();
+});
+
+test('SettingsSheet snapshot with android home set', () => {
+  const component = (
+    <Provider store={getStore({androidHome: '/my/sdk/location'})}>
+      <SettingsSheet onHide={() => {}} />
+    </Provider>
+  );
+
+  expect(renderer.create(component).toJSON()).toMatchSnapshot();
+});
