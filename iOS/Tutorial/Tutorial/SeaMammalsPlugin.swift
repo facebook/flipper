54ebb44ddab24a0b157701d7c69af469fd5c4769
Andres Suarez <asuarez@fb.com>
2019-10-11 13:44:21 -0700

    Tidy up Flipper license headers [2/2]
    Reviewed By: passy
    
    Differential Revision: D17863710
    
    fbshipit-source-id: 5b249bdd1c06d3a526b250b035b797d0c8e796eb

--- iOS/Tutorial/Tutorial/SeaMammalsPlugin.swift
+++ iOS/Tutorial/Tutorial/SeaMammalsPlugin.swift
@@ -1,16 +1,9 @@
 /*
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the MIT license found in the LICENSE
- * file in the root directory of this source tree.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
-//
-//  MyFlipperPlugin.swift
-//  Tutorial
-//
-//  Created by Pritesh Nandgaonkar on 5/3/19.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
 
 import Foundation
 import FlipperKit
