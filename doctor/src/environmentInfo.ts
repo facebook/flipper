5d17eede858d96e04ddc3fac5783f3d267f87b80
Anton Nikolaev <nikoant@fb.com>
2019-12-16 16:35:49 -0800

    Workaround for Anrdoid SDK detection not working with Java 9+ installed
    Summary: Workaround for Anrdoid SDK detection not working with Java 9+ installed
    
    Reviewed By: mweststrate
    
    Differential Revision: D19094906
    
    fbshipit-source-id: a7c8c9c8403d25ecc9b2d95113718ddf1821a349

--- doctor/src/environmentInfo.ts
+++ doctor/src/environmentInfo.ts
@@ -32,6 +32,9 @@ export type EnvironmentInfo = {
 };
 
 export async function getEnvInfo(): Promise<EnvironmentInfo> {
+  // Temporary fix for https://github.com/facebook/flipper/issues/667 until it properly fixed in 'envinfo'.
+  process.env.JAVA_OPTS =
+    '-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee';
   return JSON.parse(
     await run(
       {
