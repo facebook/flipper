a624b064a8df67676c9c23c415e2e1056a4dc4ae
Chaiwat Ekkaewnumchai <cekkaewnumchai@fb.com>
2020-03-19 05:14:48 -0700

    (Server) Fix Naming
    Summary: per title
    
    Reviewed By: mweststrate
    
    Differential Revision: D20489140
    
    fbshipit-source-id: 8678c1bfed578c5ec7138e0ccb25777942d5ed4c

--- desktop/plugins/network/RequestDetails.tsx
+++ desktop/plugins/network/RequestDetails.tsx
@@ -184,9 +184,9 @@ export default class RequestDetails extends Component<
             {response.headers.length > 0 ? (
               <Panel
                 key={'responseheaders'}
-                heading={
-                  response.isMock ? 'Response Body (Mock)' : 'Response Body'
-                }
+                heading={`Response Headers${
+                  response.isMock ? ' (Mocked)' : ''
+                }`}
                 floating={false}
                 padded={false}>
                 <HeaderInspector headers={response.headers} />
@@ -194,7 +194,7 @@ export default class RequestDetails extends Component<
             ) : null}
             <Panel
               key={'responsebody'}
-              heading={'Response Body'}
+              heading={`Response Body${response.isMock ? ' (Mocked)' : ''}`}
               floating={false}
               padded={!formattedText}>
               <ResponseBodyInspector
