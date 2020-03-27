7a40d3f0a33ce26304709ade96ca809a0eb52c33
Michel Weststrate <mweststrate@fb.com>
2020-03-27 04:39:06 -0700

    Introduce `sideEffects` for safer management of store side effects
    Summary:
    This diff introducing the sideEffect abstractions, that creates a fundamental solution for the problem in D20619226: If a subscription to the store errors, the entire store can't be updated anymore, bringing the entire app to a halt.
    
    Applying this abstraction will be done in a next diff.
    
    To essential idea of sideEffect is that it fixes a few problems:
    
    1. I decouples and throttles the effect so that no arbitrary expensive burden is added to every store update
    2. It makes sure that a crashing side effect doesn't crash the entire store update
    3. It helps with tracing and monitoring perf problems
    4. It puts the side effect behind a selector so that the side effect is only triggered if a relevant part of the store changes, like we do for components.
    
    Note that if some subscription _must_ be handled synchronously, than that logic should be in a reducer, not in a subscription or side effect. Luckily we don't have any examples of that in our code base.
    
    This abstraction might actually be intesting to be shared wider for fun and profit.
    
    Reviewed By: passy
    
    Differential Revision: D20625872
    
    fbshipit-source-id: adaf8356950594d50e6a99a17a862f757c3777db

---
+++ desktop/app/src/utils/sideEffect.tsx
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import {Store as ReduxStore} from 'redux';
+import {shallowEqual} from 'react-redux';
+
+/**
+ * Registers a sideEffect for the given redux store. Use this utility rather than subscribing to the Redux store directly, which fixes a few problems:
+ * 1. It decouples and throttles the effect so that no arbitrary expensive burden is added to every store update.
+ * 2. It makes sure that a crashing side effect doesn't crash the entire store update.
+ * 3. It helps with tracing and monitoring perf problems.
+ * 4. It puts the side effect behind a selector so that the side effect is only triggered if a relevant part of the store changes, like we do for components.
+ *
+ * @param store
+ * @param options
+ * @param selector
+ * @param effect
+ */
+export function sideEffect<
+  Store extends ReduxStore<any, any>,
+  V,
+  State = Store extends ReduxStore<infer S, any> ? S : never
+>(
+  store: Store,
+  options: {name: string; throttleMs: number},
+  selector: (state: State) => V,
+  effect: (selectedState: V, store: Store) => void,
+): () => void {
+  let scheduled = false;
+  let lastRun = 0;
+  let lastSelectedValue: V = selector(store.getState());
+  let timeout: NodeJS.Timeout;
+
+  function run() {
+    scheduled = false;
+    const start = performance.now();
+    try {
+      // Future idea: support effects that return promises as well
+      lastSelectedValue = selector(store.getState());
+      effect(lastSelectedValue, store);
+    } catch (e) {
+      console.error(
+        `Error while running side effect '${options.name}': ${e}`,
+        e,
+      );
+    }
+    lastRun = performance.now();
+    const duration = lastRun - start;
+    if (duration > 15 && duration > options.throttleMs / 10) {
+      console.warn(
+        `Side effect '${
+          options.name
+        }' took ${duration}ms, which exceeded its budget of ${Math.floor(
+          options.throttleMs / 10,
+        )}ms. Please make the effect faster or increase the throttle time.`,
+      );
+    }
+  }
+
+  const unsubscribe = store.subscribe(() => {
+    if (scheduled) {
+      return;
+    }
+    const newValue = selector(store.getState());
+    if (
+      newValue === lastSelectedValue ||
+      shallowEqual(newValue, lastSelectedValue)
+    ) {
+      return; // no new value, no need to schedule
+    }
+    scheduled = true;
+    timeout = setTimeout(
+      run,
+      // Run ASAP (but async) or, if we recently did run, delay until at least 'throttle' time has expired
+      Math.max(1, lastRun + options.throttleMs - performance.now()),
+    );
+  });
+
+  return () => {
+    clearTimeout(timeout);
+    unsubscribe();
+  };
+}
