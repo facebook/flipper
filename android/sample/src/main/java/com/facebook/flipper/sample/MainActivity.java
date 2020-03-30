c2725cbce8d51b88e73a6ffb33955936a09f7236
Pascal Hartig <phartig@rdrei.net>
2020-03-30 17:34:14 +0100

    Use an AndroidX Fragment

--- android/sample/src/main/java/com/facebook/flipper/sample/MainActivity.java
+++ android/sample/src/main/java/com/facebook/flipper/sample/MainActivity.java
@@ -8,7 +8,15 @@
 package com.facebook.flipper.sample;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.facebook.flipper.android.AndroidFlipperClient;
 import com.facebook.flipper.core.FlipperClient;
 import com.facebook.flipper.plugins.example.ExampleFlipperPlugin;
@@ -24,12 +32,28 @@ public class MainActivity extends AppCompatActivity {
     NavigationFacade.sendNavigationEvent("flipper://deep_link_activity/");
 
     final ComponentContext c = new ComponentContext(this);
-    setContentView(LithoView.create(c, RootComponent.create(c).build()));
+    // setContentView(LithoView.create(c, RootComponent.create(c).build()));
+
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+    setContentView(R.layout.fragment_host);
+    ExampleFragment fragment = new ExampleFragment();
+    fragmentTransaction.add(R.id.titles, fragment);
+    fragmentTransaction.commit();
 
     final FlipperClient client = AndroidFlipperClient.getInstanceIfInitialized();
     if (client != null) {
       final ExampleFlipperPlugin samplePlugin = client.getPluginByClass(ExampleFlipperPlugin.class);
       samplePlugin.setActivity(this);
+    }
+  }
+
+  public static class ExampleFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      return inflater.inflate(R.layout.text, container, false);
     }
   }
 }
