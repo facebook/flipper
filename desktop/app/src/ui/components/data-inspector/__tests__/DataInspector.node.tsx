cae0f98fff8a0d5f9636b93b8e03f811d97bf8cf
Anton Nikolaev <nikoant@fb.com>
2020-05-02 15:41:24 -0700

    Fix tests failing on github (#1092)
    Summary:
    Pull Request resolved: https://github.com/facebook/flipper/pull/1092
    
    Fixed tests failing on github. Our custom transformation for 'fb-stubs' is not applied for tests and jest.mocks in particular, and therefore we need to try mock both 'fb' and 'fb-stubs' object depending on where the test is running.
    
    Differential Revision: D21352524
    
    fbshipit-source-id: b97c8a22bdb25211c4c8fb1f7e0e6c69f84b700e

--- desktop/app/src/ui/components/data-inspector/__tests__/DataInspector.node.tsx
+++ desktop/app/src/ui/components/data-inspector/__tests__/DataInspector.node.tsx
@@ -10,7 +10,11 @@
 import * as React from 'react';
 import {render, fireEvent, waitFor} from '@testing-library/react';
 
-jest.mock('../../../../fb/Logger');
+try {
+  jest.mock('../../../../fb/Logger');
+} catch {
+  jest.mock('../../../../fb-stubs/Logger');
+}
 import ManagedDataInspector from '../ManagedDataInspector';
 
 const mocks = {
