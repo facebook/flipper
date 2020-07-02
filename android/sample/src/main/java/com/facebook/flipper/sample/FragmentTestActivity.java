90e4fe1f85d9635833b9c536e2b21dae170b8336
Pascal Hartig <phartig@rdrei.net>
2020-07-02 12:12:15 +0100

    [android] Add fragment test activity
    Summary:
    Wanted to have a way to test fragment support in open source.
    
    Test Plan:
    Pic here.

---
+++ android/sample/src/main/java/com/facebook/flipper/sample/FragmentTestActivity.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.flipper.sample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FragmentTestActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    NavigationFacade.sendNavigationEvent("flipper://fragment_test_activity/");
+    setContentView(R.layout.fragment_test_activity);
+  }
+}
---
+++ android/sample/src/main/java/com/facebook/flipper/sample/FragmentTestFragment.java
@@ -0,0 +1,22 @@
+package com.facebook.flipper.sample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTestFragment extends Fragment {
+
+    public FragmentTestFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_test, container, false);
+    }
+}
