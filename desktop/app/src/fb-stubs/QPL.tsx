0bd2df6800f4ed489bc1985fe4a3caa0d134caa3
Nicole Stiliyan Vukadinova <vukadinova@fb.com>
2020-07-23 05:08:04 -0700

    Add marker start and end to App to measure the startup time for Flipper (#1397)
    Summary: Pull Request resolved: https://github.com/facebook/flipper/pull/1397
    
    Reviewed By: nubbel
    
    Differential Revision: D22549700
    
    fbshipit-source-id: 2064a3ccc201b22c9782bc761b871a10dbe965ae

---
+++ desktop/app/src/fb-stubs/QPL.tsx
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+export enum QuickLogActionType {
+  START = 1,
+  SUCCESS = 2,
+  FAIL = 3,
+  CANCEL = 4,
+  CANCEL_UNLOAD = 706,
+  INTERRUPTED = 96,
+}
+
+export enum FLIPPER_QPL_EVENTS {
+  STARTUP = 52035585,
+}
+class QuickPerformanceLogger {
+  markerStart(
+    _markerId: number,
+    _instanceKey?: number,
+    _timestamp?: DOMHighResTimeStamp,
+  ): void {}
+
+  markerPoint(
+    _markerId: number,
+    _name: string,
+    _data?: string,
+    _instanceKey?: number,
+    _timestamp?: DOMHighResTimeStamp,
+  ): void {}
+
+  markerEnd(
+    _markerId: number,
+    _action: QuickLogActionType,
+    _instanceKey?: number,
+    _timestamp?: DOMHighResTimeStamp,
+  ): void {}
+}
+
+export default new QuickPerformanceLogger();
