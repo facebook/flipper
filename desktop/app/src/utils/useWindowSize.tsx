d7a6356fb6bfbb8ffa7f8774f8e09f8252389d6e
John Knox <jknox@fb.com>
2020-07-24 07:11:49 -0700

    Add Popover2 component
    Summary:
    The current Popover component isn't compatible with the new (in progress) component library because it relies on overflowing container elements, which isn't allowed in the generic Layout component.
    
    So this is a new Popover element, which uses absolute positioning instead. It takes heavy inspiration from the Tooltip and TooltipProvider components which do a similar thing.
    
    Still to do:
    [x] Edge cases when popover would be near a window edge
    [x] Style it to look nice
    [x] Split the use case (RatingButton) changes into a separate diff
    [x] When the location of the popover container moves (the rating button in this case, e.g. if you resize the window), it doesn't currently cause the effect function in the popover, so it doesn't get moved when it should
    [x] Add a little pointer thingy like a speech bubble
    [x] Make sure it's perfectly positioned
    [ ] Rename it to Popover and delete the old one. Not done, since it's just a stopgap.
    
    Reviewed By: mweststrate
    
    Differential Revision: D22693105
    
    fbshipit-source-id: bc141433914bc20da48f8ae96764a95f7cd74ce5

---
+++ desktop/app/src/utils/useWindowSize.tsx
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import {useEffect, useState} from 'react';
+
+const isClient = typeof window === 'object';
+
+function getSize() {
+  return {
+    width: isClient ? window.innerWidth : undefined,
+    height: isClient ? window.innerHeight : undefined,
+  };
+}
+
+export function useWindowSize() {
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    if (!isClient) {
+      return;
+    }
+
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+}
