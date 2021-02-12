f639aadbd97904f1b29eda998597ad2bbb3582cd
Pascal Hartig <phartig@rdrei.net>
2021-02-12 17:54:22 +0000

    [android] Cache gradle artifacts in GH Action

---
+++ scripts/checksum-android.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+RESULT_FILE=$1
+
+if [ -f $RESULT_FILE ]; then
+  rm $RESULT_FILE
+fi
+touch $RESULT_FILE
+
+checksum_file() {
+  echo $(openssl sha256 $1 | awk '{print $2}')
+}
+
+FILES=()
+while read -r -d ''; do
+	FILES+=("$REPLY")
+done < <(find . -type f \( -name "build.gradle*" -o -name "gradle-wrapper.properties" \) -print0)
+
+# Loop through files and append checksum to result file
+for FILE in ${FILES[@]}; do
+	echo $(checksum_file $FILE) >> $RESULT_FILE
+done
+
+sort $RESULT_FILE -o $RESULT_FILE
