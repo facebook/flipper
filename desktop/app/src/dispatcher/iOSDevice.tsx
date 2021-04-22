9e7a4559107a312474366e2e885a645747169d95
Pascal Hartig <realpassy@fb.com>
2021-04-22 07:44:17 -0700

    Mute port forwarding errors
    Summary: Mentioned in the comment, these aren't actionable errors.
    
    Reviewed By: jknoxville
    
    Differential Revision: D27910465
    
    fbshipit-source-id: 6f5e9d9d25edbb02648f3118c074e9f1986e3a86

--- desktop/app/src/dispatcher/iOSDevice.tsx
+++ desktop/app/src/dispatcher/iOSDevice.tsx
@@ -64,12 +64,13 @@ function forwardPort(port: number, multiplexChannelPort: number) {
     portforwardingClient,
     [`-portForward=${port}`, `-multiplexChannelPort=${multiplexChannelPort}`],
     (err, stdout, stderr) => {
-      console.error('Port forwarding app failed to start', err, stdout, stderr);
+      // This happens on app reloads and doesn't need to be treated as an error.
+      console.warn('Port forwarding app failed to start', err, stdout, stderr);
     },
   );
   console.log('Port forwarding app started', childProcess);
   childProcess.addListener('error', (err) =>
-    console.error('Port forwarding app error', err),
+    console.warn('Port forwarding app error', err),
   );
   childProcess.addListener('exit', (code) =>
     console.log(`Port forwarding app exited with code ${code}`),
