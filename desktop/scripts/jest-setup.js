db3d0486e750e9aa48067d970d332d86ecd92f56
Michel Weststrate <mweststrate@fb.com>
2021-06-10 12:59:33 -0700

    Make sure all tests run in same TimeZone
    Summary:
    Not being able to have timestamps in Jest snapshots, as they would differ across machines required jumping through a lot of hoops.
    
    Run this into once again more by a test suite that didn't trigger on CI, but failed for me locally.
    
    With this fix everyone can happily commit time based snapshots and they will always be assumed to be running in UTC :)
    
    Reviewed By: nikoant
    
    Differential Revision: D29025759
    
    fbshipit-source-id: 426d1b065afdd4a2ed75b47203b13fff0ece1272

--- desktop/scripts/jest-setup.js
+++ desktop/scripts/jest-setup.js
@@ -11,6 +11,22 @@
 // eslint-disable-next-line
 global.fetch = require('jest-fetch-mock');
 
+// make sure test run everywhere in the same timezone!
+// +11, somewhere in the middle of nowhere, so this deviates for everyone and will fail early :)
+const timezone = 'Pacific/Pohnpei';
+if (process.env.TZ !== timezone) {
+  throw new Error(
+    `Test started in the wrong timezone, got ${process.env.TZ}, but expected ${timezone}. Please use the package.json commands to start Jest, or prefix the command with TZ=${timezone}`,
+  );
+}
+
+// Make sure we have identical formatting of Dates everywhere
+const toLocaleString = Date.prototype.toLocaleString;
+// eslint-disable-next-line no-extend-native
+Date.prototype.toLocaleString = function (_locale, ...args) {
+  return toLocaleString.call(this, 'en-US', ...args);
+};
+
 require('immer').enableMapSet();
 
 require('../app/src/fb-stubs/Logger').init(undefined, {isTest: true});
