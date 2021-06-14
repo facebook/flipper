c1d1925c4c690e81a4a5ba525c9bbfac89c3fb08
João <jneto2727@gmail.com>
2021-06-13 23:51:56 -0300

    add default initializeFlipper native function

--- react-native/ReactNativeFlipperExample/android/app/src/main/java/com/reactnativeflipperexample/MainApplication.java
+++ react-native/ReactNativeFlipperExample/android/app/src/main/java/com/reactnativeflipperexample/MainApplication.java
@@ -69,7 +69,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.reactnativeflipperexample.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.facebook.flipper.reactnative.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
