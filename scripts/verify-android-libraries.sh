7ed7769d48fca0a86d047a3db4bee11de240fcac
Pascal Hartig <phartig@rdrei.net>
2022-02-14 12:06:29 +0000

    Better error message

--- scripts/verify-android-libraries.sh
+++ scripts/verify-android-libraries.sh
@@ -4,9 +4,10 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 NUM=$(unzip -l "$BASEDIR"/android/build/outputs/aar/android-debug.aar | grep -c libevent_core)
 
 if (( NUM >= 4 )); then
-        echo "Found $NUM instances of libevent_core. Looks good."
-        exit 0
+    echo "Found $NUM instances of libevent_core. Looks good."
+    exit 0
 else
-        echo "Found fewer than 4 libevent_core.so files. Expecting one for each architecture. See #3395 for details."
-        exit 1
+    unzip -l "$BASEDIR"/android/build/outputs/aar/android-debug.aar | grep libevent_core
+    echo "Expected 4 but found $NUM libevent_core.so files. Expecting one for each architecture. See #3395 for details."
+    exit 1
 fi
