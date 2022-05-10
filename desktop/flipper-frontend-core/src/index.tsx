d795117910fef8aaeced4c07e37cf5785639daea
Andrey Goncharov <aigoncharov@fb.com>
2022-05-10 05:13:24 -0700

    Expose exports from the root of flipper-frontend-core
    Summary: flipper-frontend-core shares logic between different desktop flipper clients: flipper browser ui, flipper electron, flipper headless
    
    Reviewed By: passy
    
    Differential Revision: D36129747
    
    fbshipit-source-id: 598382ad3897701b307a16a42dc65d4c277b3e15

---
+++ desktop/flipper-frontend-core/src/index.tsx
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+export {RenderHost, getRenderHostInstance} from './RenderHost';
+export {default as AbstractClient, ClientConnection} from './AbstractClient';
+export {default as ArchivedDevice} from './devices/ArchivedDevice';
+export {default as BaseDevice} from './devices/BaseDevice';
+export * from './globalObject';
+export * from './plugins';
+export * from './flipperLibImplementation';
