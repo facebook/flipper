23ab1e91278b0888d80cc63c3cb320ce04857ab1
Luke De Feo <lukedefeo@meta.com>
2022-10-10 04:13:06 -0700

    Added value animator button
    Summary: This is usefull to make sure the ui debugger sees changes from value animators
    
    Reviewed By: lblasa
    
    Differential Revision: D40021838
    
    fbshipit-source-id: 1fe18b79a89b43f286aa4e90aa6e850db3e887a5

--- android/sample/src/main/java/com/facebook/flipper/sample/AnimationsActivity.java
+++ android/sample/src/main/java/com/facebook/flipper/sample/AnimationsActivity.java
@@ -7,19 +7,21 @@
 
 package com.facebook.flipper.sample;
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class AnimationsActivity extends Activity {
 
-  Button btnBlink, btnRotate, btnMove, btnBounce, btnSequential;
+  Button btnBlink, btnRotate, btnMove, btnBounce, btnSequential, btnValueAnimator;
   Animation animBlink, animRotate, animMove, animBounce, animSequential;
-  TextView txtBlink, txtRotate, txtMove, txtBounce, txtSeq;
+  TextView txtBlink, txtRotate, txtMove, txtBounce, txtSeq, txtValueAnimator;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +29,29 @@ public class AnimationsActivity extends Activity {
     setContentView(R.layout.activity_animations);
 
     btnBlink = findViewById(R.id.btnBlink);
+    btnBlink.setTranslationX(500);
     btnRotate = findViewById(R.id.btnRotate);
     btnMove = findViewById(R.id.btnMove);
     btnBounce = findViewById(R.id.btnBounce);
     btnSequential = findViewById(R.id.btnSequential);
+    btnValueAnimator = findViewById(R.id.btnValueAnimator);
     txtBlink = findViewById(R.id.txt_blink);
     txtRotate = findViewById(R.id.txt_rotate);
     txtMove = findViewById(R.id.txt_move);
     txtBounce = findViewById(R.id.txt_bounce);
     txtSeq = findViewById(R.id.txt_seq);
+    txtValueAnimator = findViewById(R.id.txtValueAnimator);
 
-    animBlink = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+    btnValueAnimator.setOnClickListener(
+        b -> {
+          ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 300);
+          valueAnimator.addUpdateListener(
+              animator -> txtValueAnimator.setTranslationX((Float) animator.getAnimatedValue()));
+          valueAnimator.setInterpolator(new LinearInterpolator());
+          valueAnimator.setDuration(10000);
+          valueAnimator.start();
+        });
+
     btnBlink.setOnClickListener(
         v -> {
           txtBlink.setVisibility(View.VISIBLE);
