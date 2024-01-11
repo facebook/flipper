40e38f0a6a012a4a7172e0bd680051c5dcc245bb
Fabio Carballo <fabiocarballo@meta.com>
2024-01-11 06:14:58 -0800

    Convert xplat/sonar/android/sample/src/debug/java/com/facebook/flipper/sample/FlipperInitializer to Kotlin
    Summary:
    This was an automatic conversion. We need this to be converted to Kotlin to unblock D52563388 , given we are stuck in the current litho OSS release.
    
    By converting this class to Kotlin, I can use `ComponentsConfiguration.isDebugModeEnabled` because the public API is "intact". If I had to call it from Java, `Flipper` would complain because the calling method is actually `setDebugModeEnabled` which corresponds to a breakage of the current API.
    
    Reviewed By: astreet
    
    Differential Revision: D52658356
    
    fbshipit-source-id: 7e5bd03fd2ca1e2f20468d25637bbe5f603be654

---
+++ android/sample/src/debug/java/com/facebook/flipper/sample/FlipperInitializer.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.flipper.sample
+
+import android.app.Application
+import android.content.Context
+import com.facebook.flipper.core.FlipperClient
+import com.facebook.flipper.plugins.crashreporter.CrashReporterPlugin
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.example.ExampleFlipperPlugin
+import com.facebook.flipper.plugins.fresco.FrescoFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.jetpackcompose.UIDebuggerComposeSupport
+import com.facebook.flipper.plugins.navigation.NavigationFlipperPlugin
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin.SharedPreferencesDescriptor
+import com.facebook.flipper.plugins.uidebugger.UIDebuggerFlipperPlugin
+import com.facebook.flipper.plugins.uidebugger.core.UIDContext
+import com.facebook.flipper.plugins.uidebugger.descriptors.DescriptorRegister
+import com.facebook.flipper.plugins.uidebugger.litho.UIDebuggerLithoSupport
+import com.facebook.litho.config.ComponentsConfiguration
+import com.facebook.litho.editor.flipper.LithoFlipperDescriptors
+import java.util.concurrent.TimeUnit
+import okhttp3.OkHttpClient
+import okhttp3.OkHttpClient.Builder
+
+object FlipperInitializer {
+
+  @JvmStatic
+  fun initFlipperPlugins(context: Context?, client: FlipperClient): InitializationResult {
+    val descriptorMapping = DescriptorMapping.withDefaults()
+    val networkPlugin = NetworkFlipperPlugin()
+    val interceptor = FlipperOkhttpInterceptor(networkPlugin, true)
+
+    // Normally, you would want to make this dependent on a BuildConfig flag, but
+    // for this demo application we can safely assume that you always want to debug.
+    ComponentsConfiguration.isDebugModeEnabled = true
+    LithoFlipperDescriptors.add(descriptorMapping)
+    client.addPlugin(InspectorFlipperPlugin(context, descriptorMapping))
+    client.addPlugin(networkPlugin)
+    client.addPlugin(
+        SharedPreferencesFlipperPlugin(
+            context,
+            listOf(
+                SharedPreferencesDescriptor("sample", Context.MODE_PRIVATE),
+                SharedPreferencesDescriptor("other_sample", Context.MODE_PRIVATE))))
+    client.addPlugin(FrescoFlipperPlugin())
+    client.addPlugin(ExampleFlipperPlugin())
+    client.addPlugin(CrashReporterPlugin.getInstance())
+    client.addPlugin(DatabasesFlipperPlugin(context))
+    client.addPlugin(NavigationFlipperPlugin.getInstance())
+    val descriptorRegister = DescriptorRegister.Companion.withDefaults()
+    val uidContext = UIDContext.Companion.create(context as Application)
+    UIDebuggerLithoSupport.enable(uidContext)
+    UIDebuggerComposeSupport.enable(uidContext)
+    client.addPlugin(UIDebuggerFlipperPlugin(uidContext))
+    client.start()
+    val okHttpClient =
+        Builder()
+            .addInterceptor(interceptor)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.MINUTES)
+            .build()
+    return object : InitializationResult {
+      override val okHttpClient: OkHttpClient
+        get() = okHttpClient
+    }
+  }
+
+  interface InitializationResult {
+    val okHttpClient: OkHttpClient
+  }
+}
