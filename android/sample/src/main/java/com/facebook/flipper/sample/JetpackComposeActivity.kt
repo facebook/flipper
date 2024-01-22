7860617bccf8c933281ee4102c334b6a0f99e65d
Michal Zielinski <zielinskim@meta.com>
2024-01-22 03:28:42 -0800

    Update Compose example
    Summary: This diff updates Flipper Compose example so that it's more interactive.
    
    Reviewed By: pengj
    
    Differential Revision: D52799679
    
    fbshipit-source-id: 78148e3d07f375aa1001a27af8e304e4e835c99e

--- android/sample/src/main/java/com/facebook/flipper/sample/JetpackComposeActivity.kt
+++ android/sample/src/main/java/com/facebook/flipper/sample/JetpackComposeActivity.kt
@@ -1,30 +1,60 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package com.facebook.flipper.sample
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
-@Composable
-fun MessageCard(name: String) {
-  Row(modifier = Modifier.padding(all = 8.dp)) { Text(text = "Hello $name!", minLines = 1) }
-}
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
-fun PreviewMessageCard() {
-  MessageCard("Android")
+fun Counter() {
+  var count: Int by remember { mutableIntStateOf(0) }
+
+  Column(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.spacedBy(20.dp)) {
+        Text(
+            text = "Tap + and - to update the count",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            minLines = 1)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(30.dp)) {
+              Button(onClick = { count-- }) { Text(text = "-", minLines = 1) }
+              Text(text = "$count", fontSize = 24.sp, minLines = 1)
+              Button(onClick = { count++ }) { Text(text = "+", minLines = 1) }
+            }
+      }
 }
 
 class JetpackComposeActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent { MessageCard("Flipper") }
+    setContent { Counter() }
   }
 }
