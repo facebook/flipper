9ae0d77012733091902dc70a5fba66d0bae1cec9
Luke De Feo <lukedefeo@meta.com>
2024-01-23 05:48:11 -0800

    Prevent excessive logging when myles breaks
    Summary:
    We had a large spike in scribe activity {F1355139158}
    
    This is was due a downstream issue in myles which resulted in excessive logging in the uidebugger.
    
    This is because we go thru each component and prefetch the myles lookup for each one, normally this passes and we cache the result for each component type, however when everything fails there is no caching and we hit myles over and over generating more errors
    
    Solution is to sample logs and remove duplicate logging from react query
    
    Reviewed By: lblasa
    
    Differential Revision: D53001346
    
    fbshipit-source-id: 5635165ebb2437e22b3d43756394956e1c2423d8

--- desktop/plugins/public/ui-debugger/utils/reactQuery.tsx
+++ desktop/plugins/public/ui-debugger/utils/reactQuery.tsx
@@ -19,7 +19,17 @@ setLogger({
     console.warn('[ui-debugger] ReactQuery ', ...args);
   },
   error: (...args) => {
-    //downgrade react query network errors to warning so they dont get sent to scribe
+    const messageOrError = args[0];
+    const message: string | undefined =
+      typeof messageOrError === 'string'
+        ? messageOrError
+        : messageOrError?.message;
+    if (message?.includes('Myles')) {
+      //dont log myles errors as they are already logged (with sampling and can be noisy)
+      return;
+    }
+
+    //downgrade react query network errors to warning so they dont get sent to log view
     console.warn('[ui-debugger] ReactQuery ', ...args);
   },
 });
